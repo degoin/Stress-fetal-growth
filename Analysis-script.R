83ad5cf581893b8720d24906771bc7ea40b3224d
Dana Goin <danaegoin@gmail.com>
2019-09-23 15:46:08 -0700

    Add load dataset statement

--- Analysis-script.R
+++ Analysis-script.R
@@ -1,6 +1,7 @@
 
 # stress and fetal growth analysis 
 
+load("/Users/danagoin/Documents/Research projects/CiOB-ECHO/Projects/Stress and birth weight/data/stress_fetal_growth_imp")
 
 stress_measures <- c("econ_cat", "food_cat", "job_highstrain_missing", "poor_perceived_nhbd", 
                      "low_perceived_socialstat", "discrimination",  "high_perceived_stress", 
@@ -16,7 +17,7 @@ x <- summary(pool(with(imp, glm(get(outcome) ~ get(exposure)))))["get(exposure)"
 return(x)
 }
 
-ls <- lapply(1:length(stress_measures), function(x) bivar("baby_wt_grams",stress_measures[x]))
+ls <- lapply(1:length(stress_measures), function(x) bivar("sga",stress_measures[x]))
 
 results <- data.frame(do.call(rbind, ls))
 rownames(results) <- stress_measures 
